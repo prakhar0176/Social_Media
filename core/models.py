d84b6457d132b1d8547f096a5fa2e0e32f9b4328
“Prakhar0176” <prakhar0176@gmail.com>
2023-07-03 09:23:58 +0530

    added signin, logout and account settings

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to = 'profile_images', default='blanl_profile_pic.png')
+    profileimg = models.ImageField(upload_to = 'profile_images', default='blank_profile_pic.png')
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
